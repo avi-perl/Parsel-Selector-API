6b1403dd02bd3008ca4c76ee7bf852c8f8a40f57
Avrohom Perl <info@avrohomperl.com>
2021-07-05 23:58:26 -0400

    Added scrape examples and better documentation

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from typing import Optional
 from enum import Enum, IntEnum
 
 from fastapi import FastAPI, Depends
+from fastapi.responses import HTMLResponse
 from pydantic import BaseModel, AnyUrl
 
 from util import ParselSelectorRetriever, user_agents
@@ -26,13 +27,24 @@ class ReturnStyles(str, Enum):
     BASIC = "BASIC"
     VERBOSE = "VERBOSE"
 
-    basic_keys = ["selector_item", "status_code", "status_msg", "path_data"]
-
     @classmethod
-    def make_basic(cls, sector_data):
-        for key in sector_data.__dict__.copy():
-            if key not in cls.basic_keys:
-                del sector_data.__dict__[key]
+    def make_basic(
+        cls,
+        sector_data,
+        basic_keys=["selector_item", "status_code", "status_msg", "path_data"],
+    ):
+        """Remove the keys that we do not want in a 'basic' formatted version of a SelectorData model"""
+        keys = (
+            sector_data.copy().keys()
+            if isinstance(sector_data, dict)
+            else sector_data.__dict__.copy()
+        )
+        for key in keys:
+            if key not in basic_keys:
+                if isinstance(sector_data, dict):
+                    del sector_data[key]
+                else:
+                    del sector_data.__dict__[key]
         return sector_data
 
 
@@ -46,8 +58,8 @@ class SelectorItem(BaseModel):
     class Config:
         schema_extra = {
             "example": {
-                "url": "https://aviperl.me/",
-                "path": "/html/body/div/div/main/div/div[1]/div[1]/div[2]/div/div[3]/div/a[2]/text()",
+                "url": "https://parsel-selector-api.herokuapp.com/examples/html",
+                "path": "/html/body/h1/text()",
                 "path_type": "XPATH",
                 "user_agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36",
                 "return_style": "BASIC",
@@ -77,7 +89,59 @@ class SelectorData(BaseModel):
         )
 
 
-@app.get("/")
+class DocumentExamples:
+    HTML = """
+    <html>
+        <head>
+            <title>Some HTML in here</title>
+        </head>
+        <body>
+            <h1>Look ma! HTML!</h1>
+        </body>
+    </html>
+    """.replace(
+        " ", ""
+    ).replace(
+        "\n", ""
+    )
+
+
+def get_data_response_examples():
+    """Generates the example responses for the docs while trying to be as dynamic as possible."""
+    verbose_example = {
+        "selector_item": SelectorItem.Config.schema_extra["example"],
+        "status_code": 200,
+        "status_msg": ["OK", "Request fulfilled, document follows"],
+        "path_data": "Lookma!HTML!",
+        "raw_data": DocumentExamples.HTML,
+    }
+    data_responses = {
+        200: {
+            "description": "Success",
+            "content": {
+                "application/json": {
+                    "examples": {
+                        "DATA_ONLY": {
+                            "summary": "DATA_ONLY",
+                            "value": verbose_example.get("path_data", "string"),
+                        },
+                        "BASIC": {
+                            "summary": "BASIC",
+                            "value": ReturnStyles.make_basic(verbose_example.copy()),
+                        },
+                        "VERBOSE": {
+                            "summary": "VERBOSE",
+                            "value": verbose_example,
+                        },
+                    }
+                }
+            },
+        },
+    }
+    return data_responses
+
+
+@app.get("/", responses=get_data_response_examples())
 async def get_data(selector_item: SelectorItem = Depends()):
     retriever = ParselSelectorRetriever.from_selector_item(selector_item)
     await retriever.run()
@@ -90,15 +154,24 @@ async def get_data(selector_item: SelectorItem = Depends()):
         return ReturnStyles.make_basic(data)
     elif selector_item.return_style == ReturnStyles.DATA_ONLY:
         return data.path_data
-    else: 
+    else:
         return data
+
+
+@app.get("/examples/html", response_class=HTMLResponse)
+async def return_html_example():
+    """Returns a basic HTML response for testing."""
+    print(get_data_response_examples())
+    return HTMLResponse(content=DocumentExamples.HTML, status_code=200)
 
 
 @app.get("/user_agents")
 async def get_user_agents():
+    """Returns a list of possible User-Agent examples that can be used. Useful for populating a UI that relies on this API."""
     return user_agents
+
 
 @app.get("/wake")
 async def get_user_agents():
-    """An endpoint to wake the API up when the server is asleep on services like Heroku"""
+    """An endpoint to wake the API up when the server is asleep on services like Heroku."""
     return True
