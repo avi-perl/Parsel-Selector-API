80a78ae440b85fb6e314c0402e99cc24904143fa
Avrohom Perl <info@avrohomperl.com>
2021-07-06 00:48:28 -0400

    Add example file

---
+++ examples/xpath_example.py
@@ -0,0 +1,34 @@
+import requests
+
+# Example using BASIC return style
+params = {
+    "url": "https://parsel-selector-api.herokuapp.com/examples/html",
+    "path": "/html/body/h1/text()",
+    "path_type": "XPATH",
+    "user_agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36",
+    "return_style": "BASIC",
+}
+r = requests.get("https://parsel-selector-api.herokuapp.com/", params=params)
+print(r.json())
+
+# Example using DATA_ONLY return style
+params = {
+    "url": "https://parsel-selector-api.herokuapp.com/examples/html",
+    "path": "/html/body/h1/text()",
+    "path_type": "XPATH",
+    "user_agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36",
+    "return_style": "DATA_ONLY",
+}
+r = requests.get("https://parsel-selector-api.herokuapp.com/", params=params)
+print(r.text)
+
+# Example using VERBOSE return style
+params = {
+    "url": "https://parsel-selector-api.herokuapp.com/examples/html",
+    "path": "/html/body/h1/text()",
+    "path_type": "XPATH",
+    "user_agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36",
+    "return_style": "VERBOSE",
+}
+r = requests.get("https://parsel-selector-api.herokuapp.com/", params=params)
+print(r.json())
