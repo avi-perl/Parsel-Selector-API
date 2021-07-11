8915514b6243f9a73c8cc6a15f7d7061ad459514
Avrohom Perl <info@avrohomperl.com>
2021-07-11 03:03:49 -0400

    Fix XPATH example script

--- examples/xpath_example.py
+++ examples/xpath_example.py
@@ -1,23 +1,23 @@
 import requests
 
 params = {
-    "url": "http://127.0.0.1:8000/examples/xml",
-    "path": "/note/to",
-    "path_type": "XML",
+    "url": "https://parsel-selector-api.herokuapp.com/examples/html",
+    "path": "/html/body/h1/text()",
+    "path_type": "XPATH",
 }
 
-# # Example using BASIC return style
-# params["return_style"] = "BASIC"
-# r = requests.get("https://parsel-selector-api.herokuapp.com/", params=params)
-# print(r.json(), "\n")
+# Example using BASIC return style
+params["return_style"] = "BASIC"
+r = requests.get("https://parsel-selector-api.herokuapp.com/", params=params)
+print(r.json(), "\n")
 
 # Example using DATA_ONLY return style
 params["return_style"] = "DATA_ONLY"
-r = requests.get("http://127.0.0.1:8000/", params=params)
+r = requests.get("https://parsel-selector-api.herokuapp.com/", params=params)
 print(r.text, "\n")
 
-# # Example using VERBOSE return style
-# params["return_style"] = "VERBOSE"
-# r = requests.get("https://parsel-selector-api.herokuapp.com/", params=params)
-# print(r.json(), "\n")
+# Example using VERBOSE return style
+params["return_style"] = "VERBOSE"
+r = requests.get("https://parsel-selector-api.herokuapp.com/", params=params)
+print(r.json(), "\n")
 
